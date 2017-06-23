8b8aaef87ba5d8bf2bc2d39003e3d260331f770a
AM GIMINT (Farhan Hamayun) <farhanhamayun@INT.GOV.PK>
2017-06-23 10:31:57 +0500

    Layout done

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -3,6 +3,7 @@ import { NavController, AlertController, LoadingController } from 'ionic-angular
 import { Auth, User, UserDetails, IDetailedError } from '@ionic/cloud-angular';
 import { TabsPage } from '../tabs/tabs';
 import { Http } from '@angular/http';
+import { ɵgetDOM as getDOM } from '@angular/platform-browser';
 
 @Component({
   selector: 'page-login',
@@ -31,15 +32,15 @@ export class LoginPage {
   otherwise show it
   */
   doLogin() {
-    var link = 'http://localhost:31727/WS/Login';
-        var data = JSON.stringify({username: this.username, password: this.password});
-        
-        this.http.post(link, data)
-        .subscribe(data => {
-            console.log(data);
-        }, error => {
-            console.log("Oooops!");
-        });
+    var link = 'https://physicianstat.com/WS/Login';
+    var data = JSON.stringify({ username: this.username, password: this.password });
+
+    this.http.post(link, data)
+      .subscribe(data => {
+        console.log(data);
+      }, error => {
+        console.log("Oooops!");
+    });
     this.showLogin = true;
   }
 
